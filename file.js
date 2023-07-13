0d6af8c550fab2c2d5afaf9bd6eef97e62c9e688
munteanu rita <munteanu.rita99@gmail.com>
2023-07-13 22:30:21 +0200

    compito.pt2

--- file.js
+++ file.js
@@ -1,18 +1,29 @@
 /*1.creare una funzione con dentro un loop che inizia con uno e finisce con 76*/
-
+const tabella = document.getElementsByClassName("tabellone")[0];
 const generateCell = function () {
-  const cell = document.querySelector(".cella");
   for (let i = 0; i < 76; i++) {
-    cell.innerHTML += `<div class='cella'>${i + 1}</div>`;
+    const cella = document.createElement("div");
+    cella.classList.add(`${i + 1}`);
+    cella.classList.add("cella");
+    cella.innerText = `${i + 1}`;
+    tabella.appendChild(cella);
   }
 };
 generateCell();
 
-const generaNumeri = function () {
+/*const generaNumeri = function (event) {
   const arr = [];
   for (let i = 0; i < 76; i++) {
     arr.push(i + 1);
   }
   return arr;
+};*/
+
+const generaNumeriRandom = function (generaEvent) {
+  const numeri = Math.floor(Math.random() * 76 + 1);
+  const numeri2 = parseInt(numeri) - 0;
+  const esciCella = document.getElementsByClassName(`${numeri2}`)[0];
+  esciCella.classList.add("numeroUscito");
+  console.log(esciCella);
+  return numeri2;
 };
-/*2.crea un bottono che riesca a selezionare un numero randon da 1 a 76 */
